6d6508e5f710618fe59f8abd4025d45da6be64c7
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-24 23:16:03 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,4 +15,31 @@ class BaseModel():
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, DATE_TIME_FORMAT)
+                elif key == "__class__":
+                    continue
+            
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+            
+def __str__(self):
+    """Returns a formatted string"""
+    return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+def save(self):
+    """updates the public instance attribute"""
+    self.updated_at = datetime.now()
+    models.storage.save()
+    
+def to_dict(self):
+    """ returns a dictionary containing all keys/values"""
+    new_dict = self.__dict__.copy()
+    new_dict['created_at] = new_dict['created_at].isoformat()
+    new_dict['updated_at] = new_dict['updated_at].isoformat()
+    new_dict['__class__'] = self.__class__.__name__
+    return new_dict
+        
             
