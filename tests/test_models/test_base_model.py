d3ecf1136c932f59b30ea55154be1a8003c78a0a
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-25 20:44:32 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,6 +10,16 @@ from model.base_model import BaseModel
 from datetime import datetime
 
 class Test_BaseModel(unittest.TestCase):
-    """unittest for BaseModel class"""
+    """
+    unittest for BaseModel class
+    """
     def setUp(self):
-        self.Test
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    unittest.main()
