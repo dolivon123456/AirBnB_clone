4ac6eac73b77c4e272b18ff37fe1a2bba4023145
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-26 16:22:17 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,16 @@ import models
 from datetime import datetime
 
 class BaseModel():
-    """BaseModel class"""
+    """
+    BaseModel class
+    """
     
     def __init__(self, *args, **kwargs):
-        """Initialize the class instances"""
+        """
+        Initialize the class instances
+        """
         DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
-         self.id = str(uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
@@ -25,31 +29,26 @@ class BaseModel():
         else:
             models.storage.new(self)
             
-def __str__(self):
-    """Returns a formatted string"""
-     clname = self.__class__.__name__
-     return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
-
-def save(self):
-    """updates the public instance attribute"""
-    self.updated_at = datetime.now()
-    models.storage.save()
+    def __str__(self):
+         """
+         Returns a formatted string
+         """
+            clname = self.__class__.__name__
+            return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+     
+    def save(self):
+        """
+        updates the public instance attribute
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
     
-def to_dict(self):
-    """ returns a dictionary containing all keys/values"""
-    new_dict = self.__dict__.copy()
-    new_dict['created_at'] = self.created_at.isoformat()
-    new_dict['updated_at'] = self.updated_at.isoformat()
-    new_dict['__class__'] = self.__class__.__name__
-    return new_dict
-
-
-class BaseModelEncoder(JSONEncoder):
-    """
-    JSONEncoder for base model
-    """
-    def default(self, o):
-        """default module"""
-        if isinstance(o, BaseModule):
-            return o.to_dict()
-        return supper().default(o)
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values
+        """
+        new_dict = self.__dict__.copy()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
