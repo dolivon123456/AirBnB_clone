109cbca38e224046da1d839930713173b80988e8
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-26 16:31:18 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,9 +3,14 @@
 The class FileStorage module
 """
 
-import os.path
 import json
-import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
   """ 
@@ -26,24 +31,28 @@ class FileStorage:
     """
     sets in __objects the obj with key <obj class name>.id
     """
-    cls_name = type(obj).__name__
-    obid = obj.id
-    key = str(cls_name) + "." + str(obid)
-    FileStorgae.__objects[key] = obj.to_dict()
+    ocname = obj.__class__.__name__
+    FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
     
   def save(self):
     """
     serializes __objects to the JSON file (path: __file_path)
     """
-    js_star = json.dumps(FileStorage.__objects)
-    with open(FileStorage.__file_path, 'w', encoding="UTF-8") as fd:
-            fd.write(js_str)
+    odict = FileStorage.__objects
+    objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+    with open(FileStorage.__file_path, "w") as f:
+        json.dump(objdict, f)
 
     def reload(self):
         """
         deserializes the JSON file to __objects
         """
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, encoding="UTF-8") as fd:
-                js_str = fd.readline()
-            FileStorage.__objects = json.loads(js_str)
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
