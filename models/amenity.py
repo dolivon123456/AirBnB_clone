9fd2e28a2f3abc79f4dc997fbe8f3b351a15fe47
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-26 16:55:32 +0100

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines the Amenity class.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Represent an amenity class.
+    
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
