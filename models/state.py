e5db606357d1b8acb51805350c8d350a7bae7241
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-26 16:47:53 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines the class state
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+  """
+  Represents the state class
+  
+  Attributes:
+      name (str): The name of the state.
+  """
+  
+  name = ""
