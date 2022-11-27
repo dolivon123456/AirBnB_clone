4c4ffe680e7228a300b9f0890d3e71c21c74d192
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-27 21:36:58 +0100

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+"""Defines unittests for models/amenity.py. """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """represents test amenity class module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """represents test init module """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """represents test name module """
         new = self.value()
         self.assertEqual(type(new.name), str)
