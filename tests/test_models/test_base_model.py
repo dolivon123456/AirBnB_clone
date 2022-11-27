57b1727bbaaa38bc86a44655acdc879e756eb7d2
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-27 21:35:41 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Defines unittests for models/base_model.py. """
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -8,38 +8,39 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """represents test class BaseModel module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """represents test init module """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """represents test setUp module """
         pass
 
     def tearDown(self):
+        """represents test tearDown module"""
         try:
             os.remove('file.json')
         except FileNotFoundError:
             pass
 
     def test_default(self):
-        """ """
+        """represents test default module """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """represents test kwargs module """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """represents test kwargs_int module """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -56,41 +57,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """represents test str module """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """represents test todict module """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """represents test kwargs_none module """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """represents test kwargs_one module """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ """
+        """represents test id module """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """represents test created_at module """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """represents test updated_at module """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
