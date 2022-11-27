92c5b32faf5f5d432b395cea4c38afe97d7a5b16
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-11-27 08:56:02 +0100

    Update console.py

--- console.py
+++ console.py
@@ -4,7 +4,7 @@
 import cmd
 import re
 from shlex import split
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
